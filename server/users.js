008d1a85bde308fb12b16ebc5d62ec729e0f5d5f
Tushar Garg <tshrgarg2010@gmail.com>
2022-06-14 16:50:29 +0530

    Server issue fixed

--- server/users.js
+++ server/users.js
@@ -1,26 +1,33 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+  name = name.trim();
 
-  const existingUser = users.find((user) => user.room === room && user.name === name);
+  var name1 = name;
+  var name2 = name1.charAt(0).toUpperCase() + name1.slice(1);
+  name = name2;
 
-  if(!name || !room) return { error: 'Username and room are required.' };
-  if(existingUser) return { error: 'Username is taken.' };
+  room = room.trim();
+
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  if (!name || !room) return { error: "Username and Room ID required!" };
+  if (existingUser) return { error: "Username Taken!" };
 
   const user = { id, name, room };
 
   users.push(user);
 
   return { user };
-}
+};
 
 const removeUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
 
-  if(index !== -1) return users.splice(index, 1)[0];
-}
+  if (index !== -1) return users.splice(index, 1)[0];
+};
 
 const getUser = (id) => users.find((user) => user.id === id);
 
